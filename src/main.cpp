cd4d3df29e2885db588945700b5e5796ddf20115
Logan Garcia <logangarcia@fastmail.fm>
2016-11-27 18:19:58 -0800

    implemented interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -5,78 +5,132 @@
 #include <vector>
 
 PwmOut speaker(PC_7);
-BusIn buttons(PA_10, PB_3, PB_5, PB_4);
-BusOut leds(PB_10, PA_8, PA_9);
+InterruptIn button_1(PA_10);
+InterruptIn button_2(PB_3);
+InterruptIn button_3(PB_5);
+//InterruptIn button_4(PB_4);
+DigitalOut led_1(PB_10);
+DigitalOut led_2(PA_8);
+DigitalOut led_3(PA_9);
+Serial device(UART_TX, UART_RX);
 
 std::vector <int> values;
 int num = 1;
 int compare = 0;
-int range = 4;
-bool success;
+int range = 3;
+int search = 0;
+
+void error() {
+	speaker = 0.5;
+	wait(0.25);
+	speaker = 0;
+	search = 0;
+}
 
 void check() {
 	if (compare < num) {
 		std::srand(std::time(0));
-		int random = range * (std::rand() / (RAND_MAX + 1.0));
-			
-		if (random == 1)
-			values.push_back(0x01);
-		else if (random == 2)
-			values.push_back(0x02);
-		else if (random == 3)
-			values.push_back(0x04);
-		else if (random == 4)
-			values.push_back(0x08);
-			
+		int random = std::rand() % range + 1;
+		device.printf("Random Number: %i\n", random);
+		values.push_back(random);			
 		compare++;
 	}
 }
 
 void show_pattern() {
-	leds = 0x00;
-	
 	for (int i = 0; i < values.size(); i++) {
-		if (values[i] != 0x08) {
-			leds = leds ^ values[i];
+		if (values[i] == 1) {
+			led_1 = 1;
 			wait(0.5);
-			leds = leds ^ values[i];
+			led_1 = 0;
 			wait(0.5);
 		}
-		else {
-			leds = leds ^ 0x07;
+		else if (values[i] == 2) {
+			led_2 = 1;
 			wait(0.5);
-			leds = leds ^ 0x07;
+			led_2 = 0;
+			wait(0.5);
+		}
+		else if (values[i] == 3) {
+			led_3 = 1;
+			wait(0.5);
+			led_3 = 0;
 			wait(0.5);
 		}
 	}
 }
 
-void check_input() {
-	for (int i = 0; i < values.size(); i++) {
-		if (!(buttons & 0x01)) {
-			leds = values[i];
-		}
-		else {
-			speaker = 0.5;
-			wait(0.25);
-			speaker = 0;
-			return;
-		}
-		wait(0.5);
-	}
+void on_button_1() {
+	led_1 = 1;
+	int i = button_1;
+	device.printf("%i\n", i);
 	
-	num++;
+	if (values[search] == 1 && search < values.size()) {
+		search++;
+		device.printf("Correct!\n");
+	}
+	else {
+		error();
+		device.printf("Error!\n");
+	}
+	if (search == values.size()) {
+		num++;
+		search = 0;
+		device.printf("Increment!\n");
+	}
+	wait(0.25);
+	led_1 = 0;
 }
 
-int main() {	
+void on_button_2() {
+	led_2 = 1;
+	
+	if (values[search] == 2 && search < values.size()) {
+		search++;
+		device.printf("Correct!\n");
+	}
+	else {
+		error();
+		device.printf("Error!\n");
+	}
+	if (search == values.size()) {
+		num++;
+		search = 0;
+		device.printf("Increment!\n");
+	}
+	wait(0.25);
+	led_2 = 0;
+}
+
+void on_button_3() {
+	led_3 = 1;
+	
+	if (values[search] == 3 && search < values.size()) {
+		search++;
+		device.printf("Correct!\n");
+	}
+	else {
+		error();
+		device.printf("Error!\n");
+	}
+	if (search == values.size()) {
+		num++;
+		search = 0;
+		device.printf("Increment!\n");
+	}
+	wait(0.25);
+	led_3 = 0;
+}
+
+int main() {
+	button_1.fall(&on_button_1);
+	button_2.fall(&on_button_2);
+	button_3.fall(&on_button_3);
+	//button_4.fall(&check_input);
+	device.baud(9600);
 	while(1) {		
 		check();
-		if ((!(buttons & 0x01)) || (!(buttons & 0x02)) || (!(buttons & 0x04)) || (!(buttons & 0x08))) {
-			check_input();
-		}
-		else
-			show_pattern();
-		
-		wait(1);
+		wait(2);
+		show_pattern();
 	}
 }
